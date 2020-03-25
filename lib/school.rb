28bd881069b3196fe18776751e87ec41560f8c95
Cody Alvarez <jedi.alvarez805@gmail.com>
2020-03-25 18:43:43 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,30 @@
+class School
+  attr_accessor :roster, :student, :school
+  def initialize(school)
+    @school = school
+    @roster = {}
+  end
+
+  def add_student(student, grade)
+    if @roster[grade]
+      @roster[grade] << student
+    else
+      @roster[grade] = []
+      @roster[grade] << student
+    end
+  end
+
+  def grade(student)
+    @roster[student]
+  end
+
+  def sort
+sorted_students = {}
+sorted_students = @roster.each { |grade, student| a <=> b }
+sorted_students
+
+
+  end
+end
+
 # code here!
